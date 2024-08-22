40b1f7e891342f2b6e9fff001f827a27c062be9c
jcsoares22 <juliocesarwassoares931@gmail.com>
2024-08-21 22:03:44 -0300

    commit
    commit

--- lib/feature/produto/produtos.dart
+++ lib/feature/produto/produtos.dart
@@ -1,5 +1,4 @@
 import 'package:projetocusto/components/app_drawer.dart';
-import 'package:projetocusto/components/buttons.dart';
 import 'package:flutter/material.dart';
 
 class Produtos extends StatefulWidget {
